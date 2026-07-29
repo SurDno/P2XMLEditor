c03c3ea686b846780db24b9bbd6be960b90a9043
SurDno <surdno@yandex.ru>
2026-07-29 05:19:36 +0100

    Update ExpressionParamTarget.cs

--- P2XMLEditor/GameData/VirtualMachineElements/InternalTypes/ExpressionParamTarget.cs
+++ P2XMLEditor/GameData/VirtualMachineElements/InternalTypes/ExpressionParamTarget.cs
@@ -1,24 +1,107 @@
+using System;
+using System.Linq;
 using P2XMLEditor.Core;
 using P2XMLEditor.GameData.VirtualMachineElements.Abstract;
-using P2XMLEditor.GameData.VirtualMachineElements.InternalTypes;
+using P2XMLEditor.Helper;
 using P2XMLEditor.Logging;
 
+namespace P2XMLEditor.GameData.VirtualMachineElements.InternalTypes;
+
+public enum ExpressionParamKind {
+	Param,
+	Message,
+	InputParam,
+	ObjectLiteral
+}
 
 public readonly struct ExpressionParamTarget {
-	public ParamTarget? Param { get; init; }
-	public VmElement? ObjectLiteral { get; init; }
+	public ExpressionParamKind Kind { get; init; }
 
-	public bool IsLiteral => ObjectLiteral != null;
+	public ParamTarget? Param { get; init; }
+	public MessageInfo? Message { get; init; }
+	public Event? MessageOwner { get; init; }
+	public InputParameter? InputParam { get; init; }
+	public VmElement? ObjectLiteral { get; init; }
+	public HierarchyGuid? LiteralHierarchy { get; init; }
+	public bool ByEngineGuid { get; init; }
+
+	public VmTypeInfo? ValueType { get; init; }
+
+	public bool IsLiteral => Kind == ExpressionParamKind.ObjectLiteral;
+	public bool HasLeadingPercent { get; init; }
 
 	public static ExpressionParamTarget Read(string data, VirtualMachine vm, VmElement? scope = null) {
 		if (ParamTarget.TryRead(data, vm, out var param))
-			return new() { Param = param };
-		
-		// TODO: we'll need to add support	
+			return new() {
+				Kind = ExpressionParamKind.Param,
+				Param = param,
+				ValueType = param.Parameter != null ? VmTypeHelper.GetVmTypeInfo(param.Parameter.Type, vm) : null
+			};
+
+		var leading = data.StartsWith('%');
+		var body = leading ? data[1..] : data;
+
+		if (body.Contains("_message_") && vm.TryResolveMessage(body, out var msg))
+			return new() {
+				Kind = ExpressionParamKind.Message,
+				Message = msg.Info, MessageOwner = msg.Owner,
+				ValueType = VmTypeHelper.GetVmTypeInfo(msg.Info.Type, vm),
+				HasLeadingPercent = leading
+			};
+
+		if (body.Contains("_inputparam_") && InputParameter.TryParse(body, out var ip, scope))
+			return new() {
+				Kind = ExpressionParamKind.InputParam,
+				InputParam = ip,
+				ValueType = VmTypeHelper.GetVmTypeInfo(ip!.Type, vm),
+				HasLeadingPercent = leading
+			};
+
+		if (HierarchyGuid.TryParse(body, vm, out var hierarchy))
+			return new() {
+				Kind = ExpressionParamKind.ObjectLiteral,
+				LiteralHierarchy = hierarchy,
+				ValueType = VmTypeInfo.GameObject,
+				HasLeadingPercent = leading
+			};
+
+		if (ulong.TryParse(body, out var id) && vm.GetNullableElement(id) is { } element)
+			return new() {
+				Kind = ExpressionParamKind.ObjectLiteral,
+				ObjectLiteral = element,
+				ValueType = VmTypeHelper.GetVmType(element.GetType()),
+				HasLeadingPercent = leading
+			};
+
+		if (Guid.TryParse(body, out _)) {
+			var byGuid = vm.GetElementsByType<GameObject>().FirstOrDefault(o => o.EngineTemplateId == body);
+			if (byGuid != null)
+				return new() {
+					Kind = ExpressionParamKind.ObjectLiteral,
+					ObjectLiteral = byGuid, ByEngineGuid = true,
+					ValueType = VmTypeHelper.GetVmType(byGuid.GetType()),
+					HasLeadingPercent = leading
+				};
+		}
+
 		Logger.Log(LogLevel.Error, $"Unresolved Expression TargetParam '{data}'.");
-		return new() { Param = ParamTarget.Empty() };
+		return new() { Kind = ExpressionParamKind.Param, Param = ParamTarget.Empty() };
 	}
 
-	public string Write() => Param?.Write() ?? ObjectLiteral!.Id.ToString();
+	public string Write() {
+		if (Kind == ExpressionParamKind.Param) return Param!.Value.Write();
+
+		var value = Kind switch {
+			ExpressionParamKind.Message      => Message!.Value.Name,
+			ExpressionParamKind.InputParam   => InputParam!.Name,
+			ExpressionParamKind.ObjectLiteral => LiteralHierarchy != null ? LiteralHierarchy.Write()
+				: ByEngineGuid ? (ObjectLiteral as GameObject)!.EngineTemplateId
+				: ObjectLiteral!.Id.ToString(),
+			_ => throw new InvalidOperationException($"Unhandled {nameof(ExpressionParamKind)} {Kind}")
+		};
+
+		return HasLeadingPercent ? "%" + value : value;
+	}
+
 	public override string ToString() => Write();
 }
